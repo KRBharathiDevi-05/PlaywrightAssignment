b8a7dc04ba4ea73e5398e4fdd0cad26bb31e9075
KRBharathiDevi-05 <bharathidevikr06@gmail.com>
2026-05-16 14:59:10 +0530

    Add files via upload

---
+++ Day 1/learnConst.js
@@ -0,0 +1,10 @@
+const browserVersion = "Chrome"
+function getBrowserVersion() 
+{
+    if (browserVersion === "Chrome")
+        {
+        const browserVersion = 12
+    }
+        console.log(browserVersion)
+}
+getBrowserVersion()
